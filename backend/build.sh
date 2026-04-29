6394afa8fb73ec15d5f4e08ff3097ee8fd582b07
Akhiljoji145 <akhiljoji1451@gmail.com>
2026-04-29 08:38:36 +0530

    feat: add build script to automate static collection, migrations, and server startup

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-
+pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
@@ -10,4 +10,3 @@ python manage.py migrate
 # Create admin user if it doesn't exist
 # This uses the custom command we created in users/management/commands/create_admin.py
 python manage.py create_admin
-python manage.py runserver 0.0.0.0:8000
