68bc3c0cb8085841dca1b665a365179375317864
Akhiljoji145 <akhiljoji1451@gmail.com>
2026-04-29 07:36:59 +0530

    commit

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,4 @@ python manage.py migrate
 # Create admin user if it doesn't exist
 # This uses the custom command we created in users/management/commands/create_admin.py
 python manage.py create_admin
+python manage.py runserver
